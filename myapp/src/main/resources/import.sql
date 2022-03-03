cacd24a0e8169ef680c30b7e2f2a29a67cc48ea7
Sergio <sbatsan650@g.educaand.es>
2022-03-03 13:48:11 +0100

    .

--- myapp/src/main/resources/import.sql
+++ myapp/src/main/resources/import.sql
@@ -1,18 +1,16 @@
-INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('E1', 'Medico', '111111111', '1');
-INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('E2', 'Medico', '111111111', '2');
-INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('E3', 'Medico', '111111111', '3');
-INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('E4', 'Medico', '111111111', '4');
-INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('E5', 'Medico', '111111111', '5');
-INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('E6', 'Medico', '111111111', '6');
+INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('Cirujano', 'Marcos', '654874522', 'García');
+INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('Geriatría', 'Jose', '654874592', 'Martínez');
+INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('Hematología', 'Laura', '654874822', 'López');
+INSERT INTO `medicos` (`especialidad`, `nombre`, `telefono`, `apellidos`) VALUES ('Infectología', 'Marta', '654874528', 'González');
 
-INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle1', '2022-02-17', 'Nombre1', '12321', '111111111', 'Cordoba', 'Apellido1');
-INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle2', '2022-02-17', 'Nombre2', '12321', '111111111', 'Cordoba', 'Apellido2');
-INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle3', '2022-02-17', 'Nombre3', '12321', '111111111', 'Cordoba', 'Apellido3');
-INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle4', '2022-02-17', 'Nombre4', '12321', '111111111', 'Cordoba', 'Apellido4');
-INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle5', '2022-02-17', 'Nombre5', '12321', '111111111', 'Cordoba', 'Apellido5');
-INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle6', '2022-02-17', 'Nombre6', '12321', '111111111', 'Cordoba', 'Apellido6');
+INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle1', '1987-03-19', 'Hugo', '15421', '654774822', 'Cordoba', 'García');
+INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle2', '1956-06-15', 'Martín', '16542', '654554822', 'Cordoba', 'Hernández');
+INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle3', '1988-02-27', 'Mateo', '24152', '654822822', 'Sevilla', 'Sánchez');
+INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle4', '1944-02-03', 'Daniel', '15232', '624274822', 'Cordoba', 'Pérez');
+INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle5', '1936-06-17', 'Miguel', '12514', '654822822', 'Cordoba', 'Rodríguez');
+INSERT INTO `pacientes` (`direccion`, `fecha`, `nombre`, `codigo_postal`, `telefono`, `provincia`, `apellidos`) VALUES ('Calle6', '1948-08-01', 'Marta', '14014', '654333322', 'Cordoba', 'Sánchez');
 
-INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('1', '2022-02-09', '1', '1', '1');
-INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('2', '2022-02-09', '1', '1', '2');
-INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('3', '2022-02-09', '1', '1', '3');
-INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('1', '2022-02-09', '2', '2', '1');
+INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('1', '2022-02-02', '1', '1', '1');
+INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('2', '2022-02-03', '1', '1', '2');
+INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('3', '2022-02-07', '1', '1', '3');
+INSERT INTO `ingresos` (`cama`, `fecha_ingreso`, `habitacion`, `medico_id`, `paciente_id`) VALUES ('1', '2022-02-01', '2', '2', '1');
